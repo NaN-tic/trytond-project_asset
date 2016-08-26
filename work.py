8391ed5060c8c4ba7ead77607d8a379a05d1f4cf
Guillem Barba <guillem@nan-tic.com>
2016-08-26 18:20:08 +0200

    Update translations

--- work.py
+++ work.py
@@ -1,7 +1,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 from trytond.pyson import Eval
 
 
@@ -11,10 +11,8 @@ __all___ = ['Project']
 class Project:
     __name__ = 'project.work'
     __metaclass__ = PoolMeta
-
     asset = fields.Many2One('asset', 'Asset',
         states={
             'invisible': Eval('type') != 'project',
             },
         depends=['type'])
-
