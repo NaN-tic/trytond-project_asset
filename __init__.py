26068a632bbb956d8a5d91d26ce17ab633c77f58
Raimon Esteve <raimon@nan-tic.com>
2017-10-05 11:08:29 +0200

    Upgrade from 4.0 branch

--- __init__.py
+++ __init__.py
@@ -1,12 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .work import *
+from . import work
 
 
 def register():
     Pool.register(
-        Project,
-        Contract,
-        ContractLine,
+        work.Project,
         module='project_asset', type_='model')
